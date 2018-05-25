54a9495f6f4dbf763e82877248cc9c35ae4e12b4
audreypapin <audrey.papin@optimadsi.fr>
2018-05-25 14:39:34 +0200

    Ajout_eco_taxe
    Permet d'ajouter l'ecotaxe dans les demande de prix fournisseurs et les commandes fournisseurs

---
+++ ajout_ecotax/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2010-2018 OPTIMA DSI - OPTIMA COM - OPTIMA FIBRE
+# @author: Audrey Papin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'ajout_ecotax',
+    'summary': 'ajout_ecotax',
+    'license': 'AGPL-3',
+    'version': '1',
+    'author': "Optima DSI",
+    'maintainer': 'Optima DSI',
+    'depends': ['purchase'],
+    'data': ['inherit_ecotaxe_frais_de_port.xml'],
+    'website': 'http://www.optimadsi.fr/',
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
